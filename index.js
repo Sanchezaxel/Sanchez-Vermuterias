ac0a5bc98727aa31f1f20a121715d4bc7a9112c5
Sanchezaxel <axel.digiacinto@gmail.com>
2024-02-27 12:58:28 -0300

    version final trabajo practico 1

---
+++ index.js
@@ -0,0 +1,10 @@
+script>
+    
+    function comprarVermut() {
+    var seleccion = prompt("¿Qué tipo de vermut quieres comprar? (Blanco, Rojo, Bianco)");
+
+    if (seleccion) {
+        alert("Has seleccionado comprar Vermut " + seleccion);
+    }
+}
+</script> 
